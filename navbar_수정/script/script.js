0f01b68cce88b1befde1b9bdf09a31027c2ed8d8
anyoungtae1 <139516015+anyoungtae1@users.noreply.github.com>
2023-09-06 10:16:18 +0900

    navbar_수정_230905_안영태
    navbar_수정_230905_안영태

---
+++ navbar_수정/script/script.js
@@ -0,0 +1,26 @@
+// // 모든 .nav-bar 요소들을 선택합니다.
+// const header = document.querySelectorAll(".header");
+// const navBars = document.querySelectorAll("nav");
+
+// const navMenus = document.querySelectorAll(".sub-menu");
+
+// // .nav_bg 요소를 선택합니다.
+// const navBg = document.querySelector(".nav_bg");
+
+// // 각 .nav-bar 요소에 대하여 mouseover 이벤트 리스너를 추가합니다.
+// navBars.forEach(function (navBar) {
+//   navBar.addEventListener("mouseover", function () {
+//     navBg.style.display = "block";
+//     navMenus.forEach(function (navMenu) {
+//       navMenu.style.display = "block";
+//     });
+//   });
+// });
+
+// // .nav_bg 요소에서 mouseleave 이벤트 리스너를 추가합니다.
+// navBg.addEventListener("mouseleave", function () {
+//   navBg.style.display = "";
+//   navMenus.forEach(function (navMenu) {
+//     navMenu.style.display = "";
+//   });
+// });

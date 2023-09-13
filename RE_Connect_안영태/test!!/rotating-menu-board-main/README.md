07231ce2fc0029593dc7f97df2af6230cbc14505
anyoungtae1 <139516015+anyoungtae1@users.noreply.github.com>
2023-09-13 17:31:59 +0900

    0913 매칭 test중
    0913 매칭 test중

---
+++ RE_Connect_안영태/test!!/rotating-menu-board-main/README.md
@@ -0,0 +1,6 @@
+# 회전 메뉴판 🍦
+![22](https://user-images.githubusercontent.com/105365737/191713210-ee702beb-cf04-4ecf-93a1-eb6eea752af9.gif)
+> 멋쟁이사자처럼 프론트엔드 스쿨에서 CSS 원근법 수업을 들은 후 복습을 위해 만든 회전 메뉴판입니다.
+>
+> Demo page: https://sypear.github.io/rotating-menu-board/ <br/>
+> 관련 포스팅 : https://sypear.tistory.com/21
---
+++ RE_Connect_안영태/test!!/rotating-menu-board-main/script.js
@@ -0,0 +1,18 @@
+const moveLeftButton = document.querySelector('.move-button.left'); // 왼쪽 메뉴로 이동 버튼
+const moveRightButton = document.querySelector('.move-button.right'); // 오른쪽 메뉴로 이동 버튼
+const menuList = document.querySelector('.menu-list');
+const menuItems = document.querySelectorAll('.menu-list li'); // menu-list 클래스의 자식인 li 요소들 전부 선택
+const angle = 360 / menuItems.length; // 버튼 클릭시 아이템이 회전해야 하는 각도
+let currentAngle = 0; // 현재 각도를 저장하는 변수
+
+moveLeftButton.addEventListener('click', function(e) {
+    currentAngle += angle;
+
+    menuList.style.transform = `translate(-50%, -50%) rotateY(${currentAngle}deg) `;
+});
+
+moveRightButton.addEventListener('click', function(e) {
+    currentAngle -= angle;
+
+    menuList.style.transform = `translate(-50%, -50%) rotateY(${currentAngle}deg)`;
+});
